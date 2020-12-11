37abb2caf90d52ce917d735db3d3f2edca5aa6dc
Aitor Miguel Blanco <aitormibl@gmail.com>
2020-12-12 09:20:00 +1100

    Added notation for next_move
    Signed-off-by: Aitor Miguel Blanco <aitormibl@gmail.com>

--- peasabot/peasabot.py
+++ peasabot/peasabot.py
@@ -3,12 +3,16 @@
     Our peasant bot.
 
 """
+from coderone.dungeon.agent import GameState, PlayerState
 
 
 class agent:
-    def __init__(self):
+    def __init__(self) -> None:
         self.actions = ['', 'u', 'd', 'l', 'r', 'p']
         pass
 
-    def next_move(self, game_state, player_state):
+    def next_move(self, game_state: GameState, player_state: PlayerState):
         return self.actions[0]
+
+    def reset(self):
+        pass
