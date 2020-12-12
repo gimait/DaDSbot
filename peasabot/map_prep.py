2a7896407b738650895b77834b62fdfb00368711
Aitor Miguel Blanco <aitormibl@gmail.com>
2020-12-12 22:30:13 +1100

    Added distance and bomb effect maps
    Signed-off-by: Aitor Miguel Blanco <aitormibl@gmail.com>

---
+++ peasabot/map_prep.py
@@ -0,0 +1,131 @@
+"""
+Functions to process the map information
+"""
+
+from typing import Tuple
+
+from coderone.dungeon.agent import GameState, PlayerState
+
+import numpy as np
+
+
+class DistanceMap:
+    """ Creates map of distances from player to    """
+    def __init__(self, map_size: Tuple[int, int]) -> None:
+        self.map_size = map_size
+        self.man_map = self.gen_manhattan_map(map_size)
+        self._distance_map = []
+
+    def update(self, state: GameState, player: PlayerState) -> None:
+        self.state = state
+        self.player = player.location
+        self._distance_map = self.calculate_distances()
+
+    def distance_to_point(self, point: Tuple[int, int]) -> int:
+        return self._distance_map[self.board_to_dm(point)]
+
+    def board_to_dm(self, point: Tuple[int, int]) -> int:
+        pass
+
+    def calculate_distances(self):
+        if not self._distance_map:
+            self._distance_map = self._init_distance_map()
+        else:
+            pass
+
+    def _init_distance_map(self) -> np.array:
+        # initialize distance map the first time we get one:
+        basemap = np.zeros(self.state.size)
+        for block in self.state.all_blocks:
+            basemap[block] = -1
+        # Run basic distance with dilation operation
+        basemap = self._calculate_steps(basemap, self.player)
+        return basemap
+
+    @staticmethod
+    def _calculate_steps(_map: np.array, center: Tuple[int, int]) -> np.array:
+        """ Expand value from center.
+
+            Map expects negative values for blocks and zero for free spots.
+        """
+        _map[center] = 0.1
+        done = False
+        # Add borders of map:
+        u_ones = np.full(_map.shape[1], -1)
+        v_ones = np.full((_map.shape[0] + 2, 1), -1)
+        ext_map = np.vstack([u_ones, _map, u_ones])
+        ext_map = np.hstack([v_ones, ext_map, v_ones])
+
+        while not done:
+            done = True
+            for u in range(1, ext_map.shape[0] - 1):
+                for v in range(1, ext_map.shape[1] - 1):
+                    if ext_map[u, v] == 0:
+                        visible = []
+                        if ext_map[u - 1, v] > 0:
+                            visible.append(ext_map[u - 1, v])
+                        if ext_map[u + 1, v] > 0:
+                            visible.append(ext_map[u + 1, v])
+                        if ext_map[u, v - 1] > 0:
+                            visible.append(ext_map[u, v - 1])
+                        if ext_map[u, v + 1] > 0:
+                            visible.append(ext_map[u, v + 1])
+                        if visible:
+                            done = False
+                            incr = 1 + max(visible)
+                            _map[u - 1, v - 1] = incr
+                            ext_map[u, v] = incr
+
+        for u in range(_map.shape[0]):
+            for v in range(_map.shape[1]):
+                _map[u, v] = max(0, _map[u, v])
+        return _map
+
+    @staticmethod
+    def _get_bomb_ranges(_map: np.array) -> np.array:
+        """Get map with number of blocks affected by bomb in each position.
+
+        Expects a map with 0 for free cells, -1 for undestructible blocks and 1 for destructible blocks.
+        """
+        done = False
+        # Add borders of map:
+        u_ones = np.full(_map.shape[1], -1)
+        v_ones = np.full((_map.shape[0] + 4, 1), -1)
+        ext_map = np.vstack([u_ones, u_ones, _map, u_ones, u_ones])
+        ext_map = np.hstack([v_ones, v_ones, ext_map, v_ones, v_ones])
+
+        while not done:
+            done = True
+            for u in range(1, ext_map.shape[0] - 1):
+                for v in range(1, ext_map.shape[1] - 1):
+                    if ext_map[u, v] == 0:
+                        targets = 0
+                        if ext_map[u - 1, v] > 0:
+                            targets += 1
+                        elif ext_map[u - 2, v] > 0:
+                            targets += 1
+                        if ext_map[u + 1, v] > 0:
+                            targets += 1
+                        elif ext_map[u + 2, v] > 0:
+                            targets += 1
+                        if ext_map[u, v - 1] > 0:
+                            targets += 1
+                        elif ext_map[u, v - 2] > 0:
+                            targets += 1
+                        if ext_map[u, v + 1] > 0:
+                            targets += 1
+                        elif ext_map[u, v + 1] > 0:
+                            targets += 1
+                        if targets:
+                            done = False
+                            _map[u - 1, v - 1] = targets
+        return _map
+
+    @staticmethod
+    def gen_manhattan_map(base_size: Tuple[int, int]) -> np.array:
+        """Generate a supermap that contains all possible minimum distances to points in the map"""
+        mega_map = np.zeros((base_size[0] * 2 - 1, base_size[1] * 2 - 1))
+        for u in range(mega_map.shape[0]):
+            for v in range(mega_map.shape[1]):
+                mega_map[u, v] = abs(base_size[0] - u) + abs(base_size[1] - v)
+        return mega_map
--- peasabot/peasabot.py
+++ peasabot/peasabot.py
@@ -2,16 +2,20 @@
 Our peasant bot.
 """
 from coderone.dungeon.agent import GameState, PlayerState
-from consumer_bot import Consumer_bot
 
-class agent(Consumer_bot):
+from .map_prep import DistanceMap
+
+
+class agent:
     """ Agent bot."""
     def __init__(self) -> None:
         self.actions = ['', 'u', 'd', 'l', 'r', 'p']
+        self.map_representation = DistanceMap((9, 11))
         pass
 
     def next_move(self, game_state: GameState, player_state: PlayerState):
-        return self.actions[2]
+        self.map_representation.update(game_state, player_state)
+        return self.actions[0]
 
     def reset(self):
         pass
