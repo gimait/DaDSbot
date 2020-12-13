d2e6c667a6d3325fc6cd460a1cf646120e3f8757
Aitor Miguel Blanco <aitormibl@gmail.com>
2020-12-13 12:01:07 +1100

    Reworked map_prep to stage borders of map and added new freedom map
    Signed-off-by: Aitor Miguel Blanco <aitormibl@gmail.com>

--- peasabot/map_prep.py
+++ peasabot/map_prep.py
@@ -2,52 +2,75 @@
 Functions to process the map information
 """
 
-from typing import Tuple
+from typing import Tuple, Optional
 
-from coderone.dungeon.agent import GameState, PlayerState
+from coderone.dungeon.agent import GameState
 
 import numpy as np
 
+from .utilities import get_opponents
 
-class DistanceMap:
-    """ Creates map of distances from player to all spots"""
+
+class GrMap:
     __slots__ = [
-        "distance_map",
-        "accessible_area",
+        "_map",
+        "u_border",
+        "v_border",
         "size",
         "player_pos",
         "player_id",
         "__dict__"
     ]
 
-    def __init__(self, map_size: Tuple[int, int]) -> None:
+    def __init__(self,
+                 map_size: Tuple[int, int],
+                 u_border: Optional[np.array] = None,
+                 v_border: Optional[np.array] = None) -> None:
         self.size = map_size
-        self.accessible_area = 0
-        self.distance_map = []
+        self._map = []
+        self.u_border = np.full(self.size[1], -1) if u_border is None else u_border
+        self.v_border = np.full((self.size[0] + 2, 1), -1) if v_border is None else v_border
+
+    def _initialize(self):
+        self._map = np.zeros(self.state.size)
 
     def update(self, state: GameState, player_pos: Tuple[int, int], player_id: int) -> None:
         self.state = state
         self.player_pos = player_pos
         self.player_id = player_id
-        self.distance_map, self.accessible_area = self._init_distance_map()
+        self._initialize()
+
+
+class DistanceMap(GrMap):
+    """ Creates map of distances from player to all spots"""
+    __slots__ = [
+        "accessible_area",
+    ]
+
+    def __init__(self, map_size: Tuple[int, int]) -> None:
+        super().__init__(map_size)
+        self.accessible_area = 0
 
     def distance_to_point(self, point: Tuple[int, int]) -> int:
-        return self.distance_map[point]
+        return self._map[point]
 
-    def _init_distance_map(self) -> np.array:
+    def _initialize(self) -> np.array:
         # initialize distance map the first time we get one:
         basemap = np.zeros(self.state.size)
-        for block in self.state.all_blocks:
+        for block in self.state.indestructible_blocks:
+            basemap[block] = -1
+        for block in self.state.soft_blocks:
+            basemap[block] = -1
+        for block in self.state.ore_blocks:
             basemap[block] = -1
         for bomb in self.state.bombs:
             basemap[bomb] = -1
-        for player in self.state.opponents(self.player_id):
+        for player in get_opponents(self.player_id, self.state._players):
             basemap[player]
         # Run basic distance with dilation operation
-        return self._calculate_steps(basemap, self.player_pos)
+        self._map, self.accessible_area = self._calculate_steps(basemap, self.player_pos)
 
-    @staticmethod
-    def _calculate_steps(_map: np.array, center: Tuple[int, int]) -> np.array:
+    def _calculate_steps(self, _map: np.array, center: Tuple[int, int]) -> np.array:
         """ Expand value from center.
 
             Map expects negative values for blocks and zero for free spots.
@@ -56,10 +79,8 @@ class DistanceMap:
         area = 1
         done = False
         # Add borders of map:
-        u_ones = np.full(_map.shape[1], -1)
-        v_ones = np.full((_map.shape[0] + 2, 1), -1)
-        ext_map = np.vstack([u_ones, _map, u_ones])
-        ext_map = np.hstack([v_ones, ext_map, v_ones])
+        ext_map = np.vstack([self.u_border, _map, self.u_border])
+        ext_map = np.hstack([self.v_border, ext_map, self.v_border])
 
         while not done:
             done = True
@@ -88,21 +109,14 @@ class DistanceMap:
         return _map, area
 
 
-class BombMap:
+class BombMap(GrMap):
     __slots__ = [
-        "bomb_effect_map",
-        "__dict__"
     ]
 
-    def __init__(self):
-        self.bomb_effect_map = []
+    def __init__(self, map_size):
+        super().__init__(map_size, v_border=np.full((map_size[0] + 4, 1), -1))
 
-    def update(self, state: GameState, player: PlayerState) -> None:
-        self.state = state
-        self.player = player
-        self.bomb_effect_map = self._init_bomb_effect_map()
-
-    def _init_bomb_effect_map(self) -> np.array:
+    def _initialize(self) -> np.array:
         basemap = np.zeros(self.state.size)
         for block in self.state.ore_blocks:
             basemap[block] = 1
@@ -110,19 +124,16 @@ class BombMap:
             basemap[block] = 1
         for block in self.state.indestructible_blocks:
             basemap[block] = -1
-        return self._get_bomb_ranges(basemap)
+        self._map = self._get_bomb_ranges(basemap)
 
-    @staticmethod
-    def _get_bomb_ranges(_map: np.array) -> np.array:
+    def _get_bomb_ranges(self, _map: np.array) -> np.array:
         """Get map with number of blocks affected by bomb in each position.
 
         Expects a map with 0 for free cells, -1 for undestructible blocks and 1 for destructible blocks.
         """
         # Add borders of map:
-        u_ones = np.full(_map.shape[1], -1)
-        v_ones = np.full((_map.shape[0] + 4, 1), -1)
-        ext_map = np.vstack([u_ones, u_ones, _map, u_ones, u_ones])
-        ext_map = np.hstack([v_ones, v_ones, ext_map, v_ones, v_ones])
+        ext_map = np.vstack([self.u_border, self.u_border, _map, self.u_border, self.u_border])
+        ext_map = np.hstack([self.v_border, self.v_border, ext_map, self.v_border, self.v_border])
 
         for u in range(2, ext_map.shape[0] - 2):
             for v in range(2, ext_map.shape[1] - 2):
@@ -146,6 +157,45 @@ class BombMap:
                         targets += 1
                     if targets:
                         _map[u - 2, v - 2] = targets
+        return _map
+
+
+class FreedomMap(GrMap):
+    __slots__ = [
+        "mask"
+    ]
+
+    def __init__(self, map_size):
+        super().__init__(map_size, u_border=np.full(map_size[1], 0), v_border=np.full((map_size[0] + 2, 1), 0))
+        self.mask = np.array(((1, 2, 1), (2, 0, 2), (1, 2, 1)))
+
+    def _initialize(self) -> np.array:
+        basemap = np.ones(self.state.size)
+        for block in self.state.indestructible_blocks:
+            basemap[block] = 0
+        for block in self.state.soft_blocks:
+            basemap[block] = 0
+        for block in self.state.ore_blocks:
+            basemap[block] = 0
+        for bomb in self.state.bombs:
+            basemap[bomb] = 0
+        for player in get_opponents(self.player_id, self.state._players):
+            basemap[player] = 0
+        self._map = self._grad_convolution(basemap)
+
+    def _grad_convolution(self, _map: np.array) -> np.array:
+
+        ext_map = np.vstack([self.u_border, _map, self.u_border])
+        ext_map = np.hstack([self.v_border, ext_map, self.v_border])
+
+        for u in range(1, ext_map.shape[0] - 2):
+            for v in range(1, ext_map.shape[1] - 2):
+                if ext_map[u, v] == 1:
+                    val = 0
+                    for i in range(3):
+                        for j in range(3):
+                            val += self.mask[i, j] * ext_map[u - 1 + i, v - 1 + j]
+                    _map[u - 1, v - 1] = val
         return _map
 
 
